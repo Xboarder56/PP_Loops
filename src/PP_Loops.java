9cf63ba986a8bc6e9a0a155f533d7cea054a0149
Sn0w <garrettbeasley@icloud.com>
2014-10-06 23:50:29 -0700

    Inital Upload of Project.

---
+++ src/PP_Loops.java
@@ -0,0 +1,28 @@
+/**
+ * Animated_Bubbles.java
+ * @author Garrett J. Beasley
+ * 10/06/2014
+ * Draws a House Picture
+ */
+
+
+//Imports for the program.
+import acm.program.ConsoleProgram;
+
+
+public class PP_Loops extends ConsoleProgram
+{
+	
+	public void run()
+	{
+		/**Set the size of the applet*/
+		setSize(500,400);
+		
+		/**Gathers the inputs from the user and stores them in a variable*/
+		String name = readLine("Enter your name:");
+		print(name);
+		
+		
+		
+	}
+}
